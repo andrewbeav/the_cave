a4a6928dcc9e4f00bf9186b1c910005ba1ec5294
andrewbeav <andrewbevelhymer@gmail.com>
2017-08-02 02:34:34 -0500

    Protect admin route so only admins can access it

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,26 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
 
 var router = express.Router();
+
+var currentUser = {};
+
+// Middleware for protecting route
+router.use(function(req, res, next) {
+  var token = req.body.token || req.headers['x-access-token'] || req.query.token;
+
+  if (!token) return res.json( { success: false, message: 'No token provided' } );
+
+  jwt.verify(token, req.app.get('jwtSecret'), function(err, decoded) {
+    currentUser = decoded._doc;
+    if (currentUser.admin) {
+      req.decoded = decoded;
+      next();
+    } else {
+      return res.json( { success: false, message: 'Not Admin' } );
+    }
+  });
+});
 
 /*
  Admin Tasks
@@ -9,5 +29,9 @@ var router = express.Router();
     * Delete any user - /ban/:_id
     * Delete any manifesto - /remove_manifesto/:_id
 */
+
+router.get('/', function(req, res, next) {
+  res.send('Admin API is working');
+});
 
 module.exports = router;
