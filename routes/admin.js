9e34914c9ca23059ea40ca88608a786797689742
andrewbeav <andrewbevelhymer@gmail.com>
2017-08-02 02:20:49 -0500

    Define what an admin should be able to do

--- routes/admin.js
+++ routes/admin.js
@@ -2,4 +2,12 @@ var express = require('express');
 
 var router = express.Router();
 
+/*
+ Admin Tasks
+    * See all users - GET /users
+    * Promote a user to admin - POST /promote/:_id
+    * Delete any user - /ban/:_id
+    * Delete any manifesto - /remove_manifesto/:_id
+*/
+
 module.exports = router;
